69fc8e963a63b2a3fb590882431bd82d0ffd35b6
Sandra <sandra13lovesyou@gmail.com>
2020-02-11 13:41:27 -0400

    menubar created

--- proyDiscapacidad/src/app/app.module.ts
+++ proyDiscapacidad/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MenubarComponent } from './menubar/menubar.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MenubarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
