aba79ccdbd5dc8376d8be696e60395ff1a54f899
Sandra <sandra13lovesyou@gmail.com>
2020-02-12 11:48:42 -0400

    routing added and login component

--- proyDiscapacidad/src/app/app.component.ts
+++ proyDiscapacidad/src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
+import {HomeComponent} from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes:Routes = [
+  {path:'home', component: HomeComponent}
+];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- proyDiscapacidad/src/app/app.module.ts
+++ proyDiscapacidad/src/app/app.module.ts
@@ -1,22 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {APP_BASE_HREF} from '@angular/common';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { MenubarComponent } from './menubar/menubar.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes:Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'login', component: LoginComponent}
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    MenubarComponent
+    MenubarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [HomeComponent, LoginComponent,{provide: APP_BASE_HREF, useValue : '/' }],
+  bootstrap: [AppComponent],
+  exports:[RouterModule]
 })
 export class AppModule { }
